91e9d4f7643ae354e4227fe5ede2dfa28a1afc6b
Milosz Liput <miloszliput@gmail.com>
2023-10-24 18:53:55 +0100

    Initial commit

---
+++ src/main/java/com/chessengine/chessengineservice/ChessEngineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chessengine.chessengineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessEngineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessEngineServiceApplication.class, args);
+	}
+
+}
