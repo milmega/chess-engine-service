6fbd14a20eb6c905840c04f594afda1b55b27356
Milosz Liput <miloszliput@gmail.com>
2023-12-28 12:55:11 +0100

    Added score property to move class

--- src/main/java/com/chessengine/chessengineservice/Move.java
+++ src/main/java/com/chessengine/chessengineservice/Move.java
@@ -7,6 +7,7 @@ public class Move {
     private int x;
     private int y;
     private Move destination;
+    private int score;
 
     public Move(int x, int y) {
         this.x = x;
@@ -17,6 +18,13 @@ public class Move {
         this.x = x;
         this.y = y;
         this.destination = new Move(destX, destY);
+    }
+
+    public Move(int x, int y, int destX, int destY, int score) {
+        this.x = x;
+        this.y = y;
+        this.destination = new Move(destX, destY);
+        this.score = score;
     }
 
     public void setMove(int x, int y) {
@@ -34,5 +42,21 @@ public class Move {
 
     public Move getDestination() {
         return destination;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Move getCopy() {
+        return new Move(this.getX(), this.getY(), this.getDestination().getX(), this.getDestination().getY(), this.getScore());
+    }
+
+    public String toString() {
+        return getX() + ", " + getY() + " to " + getDestination().getX() + ", " + getDestination().getY();
     }
 }
