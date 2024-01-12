6fbe5f65ae8db5c201527d5c153e67e257e76365
Milosz Liput <miloszliput@gmail.com>
2024-01-12 16:46:12 +0000

    Added Piece struct

---
+++ src/main/java/com/chessengine/chessengineservice/Piece.java
@@ -0,0 +1,15 @@
+package com.chessengine.chessengineservice;
+
+public class Piece {
+    public static final int EMPTY = 0;
+    public static final int PAWN = 1;
+    public static final int KNIGHT = 2;
+    public static final int BISHOP = 3;
+    public static final int ROOK = 4;
+    public static final int QUEEN = 5;
+    public static final int KING = 6;
+
+    public Piece() {
+
+    }
+}
