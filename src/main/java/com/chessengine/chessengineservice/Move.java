a10d73bad102c01a400cf10c681c8e2e46bd1bd8
Milosz Liput <miloszliput@gmail.com>
2024-01-12 17:58:25 +0000

    Refactored Move Class and added MoveDetails to help with making ang unmaking moves.

--- src/main/java/com/chessengine/chessengineservice/Move.java
+++ src/main/java/com/chessengine/chessengineservice/Move.java
@@ -1,62 +1,15 @@
 package com.chessengine.chessengineservice;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Move {
-    private int x;
-    private int y;
-    private Move destination;
-    private int score;
+    public int currentSquare;
+    public int targetSquare;
 
-    public Move(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public Move(int x, int y, int destX, int destY) {
-        this.x = x;
-        this.y = y;
-        this.destination = new Move(destX, destY);
-    }
-
-    public Move(int x, int y, int destX, int destY, int score) {
-        this.x = x;
-        this.y = y;
-        this.destination = new Move(destX, destY);
-        this.score = score;
-    }
-
-    public void setMove(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return  x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public Move getDestination() {
-        return destination;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
+    public Move(int currentSquare, int targetSquare) {
+        this.currentSquare = currentSquare;
+        this.targetSquare = targetSquare;
     }
 
     public Move getCopy() {
-        return new Move(this.getX(), this.getY(), this.getDestination().getX(), this.getDestination().getY(), this.getScore());
-    }
-
-    public String toString() {
-        return getX() + ", " + getY() + " to " + getDestination().getX() + ", " + getDestination().getY();
+        return new Move(this.currentSquare, this.targetSquare);
     }
 }
---
+++ src/main/java/com/chessengine/chessengineservice/MoveDetails.java
@@ -0,0 +1,48 @@
+package com.chessengine.chessengineservice;
+
+public class MoveDetails {
+
+    private int targetPiece;
+    private int whiteKingPosition;
+    private int blackKingPosition;
+    private boolean[] whiteCastling;
+    private boolean[] blackCastling;
+    private Pair<Integer, Integer> rookMove;
+
+    public MoveDetails(int targetPiece, int whiteKingPosition, int blackKingPosition, boolean[] whiteCastling, boolean[] blackCastling) {
+        this.targetPiece = targetPiece;
+        this.whiteKingPosition = whiteKingPosition;
+        this.blackKingPosition = blackKingPosition;
+        this.whiteCastling = whiteCastling.clone(); //TODO: should it be with clone
+        this.blackCastling = blackCastling.clone();
+        rookMove = new Pair<>(-1, -1);
+    }
+
+    public boolean[] getBlackCastling() {
+        return blackCastling;
+    }
+
+    public boolean[] getWhiteCastling() {
+        return whiteCastling;
+    }
+
+    public int getBlackKingPosition() {
+        return blackKingPosition;
+    }
+
+    public int getWhiteKingPosition() {
+        return whiteKingPosition;
+    }
+
+    public int getTargetPiece() {
+        return targetPiece;
+    }
+
+    public Pair<Integer, Integer> getRookMove() {
+        return rookMove;
+    }
+
+    public void setRookMove(Pair<Integer, Integer> move) {
+        rookMove = new Pair<>(move.first, move.second);
+    }
+}
