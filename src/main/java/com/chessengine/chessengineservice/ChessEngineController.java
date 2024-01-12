b08099741c3ac1d3d6fb14a633b5c283071e6791
Milosz Liput <miloszliput@gmail.com>
2024-01-12 18:11:22 +0000

    Refactored engineController to take moves as param and not entire board.

--- src/main/java/com/chessengine/chessengineservice/ChessEngineController.java
+++ src/main/java/com/chessengine/chessengineservice/ChessEngineController.java
@@ -1,9 +1,6 @@
 package com.chessengine.chessengineservice;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ChessEngineController {
@@ -12,27 +9,16 @@ public class ChessEngineController {
 
     @GetMapping("/move")
     @CrossOrigin(origins = "http://localhost:3000")
-    public String GetNextMove(@RequestParam String board,
-                              @RequestParam int colour,
-                              @RequestParam int whiteKing,
-                              @RequestParam int blackKing,
-                              @RequestParam String whiteCastling,
-                              @RequestParam String blackCastling) {
-        GameEngine.setKingPosition(1, whiteKing / 8, whiteKing % 8);
-        GameEngine.setKingPosition(-1, blackKing / 8, blackKing % 8);
-        GameEngine.setCastling(1, whiteCastling);
-        GameEngine.setCastling(-1, blackCastling);
-        int[][] board2D = new int[8][8];
-        int index = 0;
-        for (int i = 0; i < board.length(); i++) {
-            String character = board.substring(i, i+1);
-            if(character.equals("-")) {
-                i++;
-                character = character.concat(board.substring(i, i+1));
-            }
-            board2D[index/8][index%8] = Integer.parseInt(character);
-            index++;
-        }
-        return chessEngineService.GetNextMove(colour, board2D);
+    public String GetNextMove(@RequestParam int start,
+                              @RequestParam int destination,
+                              @RequestParam int colour) {
+        chessEngineService.getBoard().makeMove(new Move(start, destination), false); //TODO: there is inconsistency between front-end board and backend board
+        return chessEngineService.GetNextMove(colour);
+    }
+
+    @PostMapping("/reset")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public void Reset() {
+        chessEngineService.getBoard().initializeBoard();
     }
 }
