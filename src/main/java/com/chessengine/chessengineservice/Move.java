e79bddae4b6cd45e2f3460f8a3ac8a386e708a0c
Milosz Liput <miloszliput@gmail.com>
2024-02-05 12:41:25 +0000

    Modified Move object to include move flags

--- src/main/java/com/chessengine/chessengineservice/Move.java
+++ src/main/java/com/chessengine/chessengineservice/Move.java
@@ -1,45 +1,64 @@
 package com.chessengine.chessengineservice;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import static com.chessengine.chessengineservice.Helpers.BoardHelper.posToX;
+import static com.chessengine.chessengineservice.Helpers.BoardHelper.posToY;
+
 public class Move {
     public int colour;
-    public int currentSquare;
+    public int piece;
+    public int startSquare;
     public int targetSquare;
+    public int targetPiece;
+    public int fromX;
+    public int fromY;
+    public int toX;
+    public int toY;
     public int changeX;
     public int changeY;
+    public boolean pawnTwoSquaresMove;
     public boolean promotionFlag;
     public boolean enpassantFlag;
     public int enpassantPosition;
     public boolean castlingFlag;
-    public int preCastlinPosition;
+    public int preCastlingPosition;
     public int postCastlingPosition;
+    public int gameResult;
 
-    public Move(int currentSquare, int targetSquare, int colour) { //TODO: update flags earlier in the move generation
-        this.currentSquare = currentSquare;
+    public Move(int piece, int startSquare, int targetSquare, int targetPiece) {
+        this.colour = piece > 0 ? 1 : -1;
+        this.piece = piece;
+        this.startSquare = startSquare;
         this.targetSquare = targetSquare;
-        this.colour = colour;
+        this.targetPiece = targetPiece;
+        this.pawnTwoSquaresMove = false;
         this.promotionFlag = false;
         this.enpassantFlag = false;
         this.enpassantPosition = -1;
         this.castlingFlag = false;
-        this.preCastlinPosition = -1;
+        this.preCastlingPosition = -1;
         this.postCastlingPosition = -1;
-        int fromX = currentSquare / 8;
-        int fromY = currentSquare % 8;
-        int toX = targetSquare / 8;
-        int toY = targetSquare % 8;
+        this.fromX = posToX(startSquare);
+        this.fromY = posToY(startSquare);
+        this.toX = posToX(targetSquare);
+        this.toY = posToY(targetSquare);
         this.changeX = toX - fromX;
         this.changeY = toY - fromY;
+        this.gameResult = 0;
     }
 
+    @JsonIgnore
     public Move getCopy() {
-        Move copy = new Move(this.currentSquare, this.targetSquare, this.colour);
+        Move copy = new Move(this.piece, this.startSquare, this.targetSquare, this.targetPiece);
         copy.promotionFlag = this.promotionFlag;
         copy.enpassantFlag = this.enpassantFlag;
         copy.enpassantPosition = this.enpassantPosition;
         copy.castlingFlag = this.castlingFlag;
-        copy.preCastlinPosition = this.preCastlinPosition;
+        copy.preCastlingPosition = this.preCastlingPosition;
         copy.postCastlingPosition = this.postCastlingPosition;
-
+        copy.pawnTwoSquaresMove = this.pawnTwoSquaresMove;
+        copy.gameResult = this.gameResult;
         return copy;
     }
 }
