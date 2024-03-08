c6ccda59205ba6ec77d9084c88fbec87aa613b69
Milosz Liput <miloszliput@gmail.com>
2024-03-08 23:46:13 +0000

    Modified dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM openjdk:21
-WORKDIR /app
-COPY target/chess-engine-service.jar /app/chess-engine-service.jar
+VOLUME /tmp
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "chess-engine-service.jar"]
+ARG JAR_FILE=target/chess-engine-service.jar
+ADD ${JAR_FILE} app.jar
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/chessengine/chessengineservice/ChessEngineController.java
+++ src/main/java/com/chessengine/chessengineservice/ChessEngineController.java
@@ -9,50 +9,50 @@ public class ChessEngineController {
     ChessEngineService chessEngineService = new ChessEngineService();
 
     @GetMapping("/move")
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin
     public Move getBestMove(@RequestParam int id, @RequestParam int colour) {
         return chessEngineService.getBestMove(id, colour);
     }
 
     @GetMapping("/allMoves")
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin
     public @ResponseBody List<Move> getAllMoves(@RequestParam int id, @RequestParam int colour) {
         return chessEngineService.getAllMoves(id, colour);
     }
 
     @PostMapping("/makeMove")
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin
     public int makeMove(@RequestParam int id, @RequestBody Move move) {
         return chessEngineService.makeMove(id, move);
     }
 
     @GetMapping("/newGame")
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin
     public int startNewGame(@RequestParam int colour, @RequestParam int playerId) {
         return chessEngineService.createGameOrJoinQueue(colour, playerId);
     }
 
     @GetMapping("/fetchMove")
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin
     public Move getLastMove(@RequestParam int gameId) {
         return chessEngineService.getLastMove(gameId);
     }
 
     @GetMapping("/isGameLive")
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin
     public boolean isGameLive(@RequestParam int gameId) {
         return chessEngineService.isGameLive(gameId);
     }
 
     @PostMapping("/cancelSearch")
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin
     public void cancelSearch(@RequestParam int id) {
         chessEngineService.cancelSearch(id);
     }
 
 
     @PostMapping("/reset")
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin
     public void reset(@RequestParam int id) {
         if (id == 0) {
             chessEngineService.resetGame(id);
@@ -62,7 +62,7 @@ public class ChessEngineController {
     }
 
     @GetMapping("/generateId")
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin
     public int generateId() {
         return chessEngineService.generatePlayerId();
     }
